6208f54d8cc166c6c83d9ddaa9c27b10ae6413e0
alejandroariaszuluaga <alejandroariaszuluaga@gmail.com>
2021-02-15 22:52:27 -0500

    Added support for BW images

--- yolo_webcam.py
+++ yolo_webcam.py
@@ -9,9 +9,10 @@ import os
 cam = cv2.VideoCapture(-1) #0=front-cam, 1=back-cam
 cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1300)
 cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 1500)
+CV_LOAD_IMAGE_GRAYSCALE = 0
 
 # Load Yolo
-net = cv2.dnn.readNet("yolo_weights/yolov4-tiny_training_20000.weights", "config/yolov4-tiny_testing.cfg")
+net = cv2.dnn.readNet("yolo_weights/yolov4-tiny_training_bw3_last.weights", "config/yolov4-tiny_testing.cfg")
 # net = cv2.dnn.readNet("yolo_weights/yolov3_training_last.weights", "config/yolov3_testing.cfg")
 
 # Name custom object
@@ -20,19 +21,17 @@ classes = ["fish"]
 # Images path
 cwd_0 = os.getcwd()
 img_folder_path = os.path.join(cwd_0, 'test_images')
-print(img_folder_path)
+img_path = os.path.join(img_folder_path, 'img1.jpg')
+# print(img_folder_path)
 # images_path = glob.glob(r"{}/*.jpg".format(img_folder_path))
 layer_names = net.getLayerNames()
 output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 
-# imagen = cv2.imread('/home/alejo/Documents/Piscicultura/fish_counting/test_images/A000001_R_00000010.jpg')
-
-
-
 def detect_fish(imagen):
-    colors = np.random.uniform(0, 255, size=(len(classes), 3))
+    colors = np.random.uniform(0, 255, size=(len(classes)))
     img = imagen
     # img = cv2.resize(imagen, None, fx=0.4, fy=0.4)
+    print(img.shape)
     height, width, channels = img.shape
 
     # Detecting objects
@@ -53,8 +52,9 @@ def detect_fish(imagen):
             confidence = scores[class_id]
             if confidence > 0.3:
                 # Object detected
+                print('####### DETECTION #######')
+                print(detection, '\n')
                 peces += 1
-                
                 center_x = int(detection[0] * width)
                 center_y = int(detection[1] * height)
                 w = int(detection[2] * width)
@@ -86,10 +86,15 @@ def detect_fish(imagen):
 while True:
     ## read frames
     ret, imagen = cam.read()
+    # print(img_path)
+
+    # imagen = cv2.imread(img_path, CV_LOAD_IMAGE_GRAYSCALE) # Test with image in directory test_images/
+    # imagen = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
+
     ## Fish Detection
     boxed_image = detect_fish(imagen)
-    cv2.imshow('', boxed_image)
-    ## press q or Esc to quit    
+    cv2.imshow('Yolo in action', imagen)
+    ## press q or Esc to quit
     if (cv2.waitKey(1) & 0xFF == ord("q")) or (cv2.waitKey(1)==27):
         break
 
